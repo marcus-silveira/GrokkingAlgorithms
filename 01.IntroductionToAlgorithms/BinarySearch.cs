7a8c9aab56eeef91c0c3c76b06ac5833c8057697
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-04-30 10:05:34 -0300

    Iniciado capitulo 1

---
+++ 01.IntroductionToAlgorithms/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.IntroductionToAlgorithms;
+
+public class BinarySearch
+{
+    public static int? BinarySearchMethod(IList<int> list, int item)
+    {
+        var low = 0;
+        var high = list.Count - 1;
+
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            var guess = list[mid];
+
+            if (guess == item) return mid;
+            if (guess > item)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return null;
+    }
+}
---
+++ 01.IntroductionToAlgorithms/Program.cs
@@ -0,0 +1,11 @@
+﻿using _01.IntroductionToAlgorithms;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var myList = new List<int> { 1, 3, 5, 7, 9 };
+        Console.WriteLine(BinarySearch.BinarySearchMethod(myList, 3));
+        Console.WriteLine(BinarySearch.BinarySearchMethod(myList, -1));
+    }
+}
